2028dd28ada48d94fc3ca56804a3a8fd99a73667
Hanna Reis <reishanna157@gmail.com>
2026-09-10 21:05:05 -0400

    chore: alinha nome do banco padrao com pro_link_db

--- _config.php
+++ _config.php
@@ -31,7 +31,7 @@ return [
     'database' => [
         'host' => $_ENV['DB_HOST'] ?? 'mariadb',
         'port' => $_ENV['DB_PORT'] ?? '3306',
-        'database' => $_ENV['DB_DATABASE'] ?? 'prolink',
+        'database' => $_ENV['DB_DATABASE'] ?? 'pro_link_db',
         'username' => $_ENV['DB_USERNAME'] ?? 'prolink',
         'password' => $_ENV['DB_PASSWORD'] ?? '',
     ],
